369c153c733242e5517138346e658f5a6e6a10af
Nikita Jain <jnikita356@gmail.com>
2021-04-17 14:16:37 +0530

    added faqs and resources content

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
@@ -45,7 +45,7 @@ class Header extends Component {
         return (
             <div>
                 <Navbar dark expand="md">
-                <div className="container">
+                    <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
                         <NavbarBrand className="mr-auto" href="/">
                             <img
@@ -58,19 +58,19 @@ class Header extends Component {
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/pricing">Pricing
+                                    <NavLink className="nav-link" to="/pricing"> Pricing
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/services">Services
+                                    <NavLink className="nav-link" to="/services"> Services
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/resources">Resources
+                                    <NavLink className="nav-link" to="/resources"> Resources
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/faqs">FAQs
+                                    <NavLink className="nav-link" to="/faqs"> FAQs
                                     </NavLink>
                                 </NavItem>
                             </Nav>
@@ -84,20 +84,7 @@ class Header extends Component {
                         </Collapse>
                     </div>
                 </Navbar>
-                <Jumbotron>
-                    <div className="container">
-                        <div className="row row-header">
-                            <div className="col-12 col-sm-6">
-                                <h1>Kiraana Haat</h1>
-                                <p>
-                                    We take inspiration from the World's best cuisines, and create
-                                    a unique fusion experience. Our lipsmacking creations will
-                                    tickle your culinary senses!
-                    </p>
-                            </div>
-                        </div>
-                    </div>
-                </Jumbotron>
+                
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
                     <ModalBody>
--- src/components/ResourcesComponent.js
+++ src/components/ResourcesComponent.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 function Resources(props) {
     return(
         <div className="container">
-            <h4>Resources</h4>
+            <h3>Welcome to selling with Kiraana-Haat!!!</h3><br></br>
+            <h5>We are more than happy to be partnered with you. </h5><br></br>
+            <h4>Let’s begin with, what is Kiraana-Haat?</h4>
+            <p>Kiraana-Haat is a start-up that aspires to be an ecommerce giant in the upcoming years.<br></br> With your help, it seems possible now.<br></br> Kiraana-Haat’s mission is to be the fastest grocery delivery platform throughout the country.</p>
+            <h4>What's in it for you?</h4>
+            <p>We work with a seller oriented approach, we make sure you get recognized for the good quality of your products. <br></br>You don't need to worry about managing your deliveries anymore, leave that to us, we will take care of it.<br></br>We help you keep track of your growth, after all business is all about growing.<br></br>Keep track of your order history, as well as returns and exchanges.</p>
+            <h4>Registration Process.</h4>
+            <p>Kiraana-Haat provides the sellers a really simple way of registration, just go to "Login", click on "Register here", fill in some basic details (it is mandatory to have a GSTIN), and done you are a partner with Kiraana-Haat now.</p>
+            <h4>Payment Process.</h4>
+            <p>You can select out of any of our packages as per your requirement.<br></br>Payments can be made online using NetBanking.</p>
+            <h6>For any queries, head to our FAQS sections, if your query still remains unresolved contact our support team. </h6>
         </div>
     );
 }
